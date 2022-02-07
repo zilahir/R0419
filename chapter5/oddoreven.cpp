1b9e604c95dc4c4da1c5723fdd0dd2f02a53722b
Richard Zilahi <richard.zilahi@varjo.com>
2022-02-07 12:34:01 +0200

    added oddoreven

---
+++ chapter5/oddoreven.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void is_odd_or_even(int n) {
+    if ( n % 2 == 0) {
+        cout << "The number " << n << " is even." << endl;
+    }
+  else {
+    cout << "The number " << n << " is odd." << endl;
+  }
+}
+
+int main()
+{
+  int number;
+
+  cout << "Input an integer:";
+  cin >> number;
+  
+  is_odd_or_even(number);
+}
